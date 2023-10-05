d0cabfe47eaa60c4f9f47e61b997ebd07eaeb0e2
akino <akikoba2001@gmail.com>
2023-10-05 17:59:06 +0900

    make sample

---
+++ main.py
@@ -0,0 +1,15 @@
+class Main:
+    def __init__(self):
+        self.start()
+        self.end()
+
+    def start(self):
+        print("Hello world!")
+        self.devider()
+
+    def end(self):
+        print("Goodbye world!")
+        self.devider()
+
+    def devider(self):
+        print("=========================================")
